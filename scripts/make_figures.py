858eef28f780337e1853cfc7a1bcb10b44475227
Jan Schleicher <jan.schleicher@uni-tuebingen.de>
2025-11-28 11:40:39 +0100

    New figure one for combined paper.

--- scripts/make_figures.py
+++ scripts/make_figures.py
@@ -1,22 +1,42 @@
 import os
 import string
-import pandas as pd
+from typing import Iterable, Optional
+
 import matplotlib as mpl
-from matplotlib import pyplot as plt
-import seaborn as sns
+import numpy as np
+import pandas as pd
 import scanpy as sc
 import scvelo as scv
-from typing import Optional
-
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 # Set up plotting parameters
-sc.set_figure_params(vector_friendly=True, dpi_save=300, scanpy=False, fontsize=10)
-scv.set_figure_params(vector_friendly=True, dpi_save=300, fontsize=10)
+sc.set_figure_params(vector_friendly=True, dpi_save=400, scanpy=False, fontsize=10)
+scv.set_figure_params(vector_friendly=True, dpi_save=400, fontsize=10)
 plt.style.use("matplotlibrc.txt")
 AXLAB_KWS = {"fontsize": 12, "fontweight": "bold", "va": "top", "ha": "left"}
-FIG_WIDTH = 17.8 / 2.54  # in inches
-FIG_WIDTH_SINGLE = 8.6 / 2.54  # in inches
+BOXPLOT_PROPS = {
+    prop: {"edgecolor" if prop == "boxprops" else "color": "k"}
+    for prop in ["boxprops", "medianprops", "whiskerprops", "capprops"]
+}
+FIG_WIDTH = 19.05 / 2.54  # in inches
+FIG_WIDTH_SINGLE = 13.2 / 2.54  # in inches
+FIG_HEIGHT_MAX = 22.23 / 2.54
 FIG_DIR = "../figures"
+PIL_KWARGS = {"compression": "tiff_lzw"}  # needed if saving as TIFF
+# Paul Tol's muted palette (https://sronpersonalpages.nl/~pault/)
+PALETTE = [
+    "#CC6677",
+    "#332288",
+    "#DDCC77",
+    "#117733",
+    "#88CCEE",
+    "#882255",
+    "#44AA99",
+    "#999933",
+    "#AA4499",
+    "#DDDDDD",
+]
 
 DATASETS_ORDER = ["pancreas", "retina", "stewart", "fu"]
 DATASETS_LABELS = {
@@ -24,26 +44,36 @@ DATASETS_LABELS = {
     "retina": "Retina",
     "stewart": "B cells",
     "fu": "T cells",
+    "fu_pe": "T cells (PE)",
+    "fu_se": "T cells (SE)",
 }
 METHODS_ORDER = [
     "velocyto",
     "alevin-fry",
+    "starsolo",
     "tidesurf",
     "cellranger",
     "tidesurf_velocyto",
     "tidesurf_alevin-fry",
     "velocyto_alevin-fry",
 ]
+METHODS_DICT = {
+    "velocyto": "velocyto",
+    "alevin-fry": "alevin-fry",
+    "starsolo": "STARsolo",
+    "tidesurf": "tidesurf",
+    "cellranger": "Cell Ranger",
+}
 SEQ_TYPES = ["3'", "5'"]
 SPLICE_STATES = ["spliced", "unspliced", "ambiguous"]
 
 
-def read_dataframes(name: str):
+def read_dataframes(name: str, datasets: Iterable = DATASETS_ORDER):
     df = (
         pd.concat(
             {
                 dataset: pd.read_parquet(f"../out/{dataset}/{name}.parquet.gz")
-                for dataset in DATASETS_ORDER
+                for dataset in datasets
             },
             names=["dataset", "idx"],
         )
@@ -76,6 +106,7 @@ def split_boxplot(
             palette=palette,
             showfliers=False,
             ax=axs[i],
+            **BOXPLOT_PROPS,
         )
         axs[i].text(
             0.5, 1, seq_type, ha="center", va="bottom", transform=axs[i].transAxes
@@ -95,6 +126,407 @@ def split_boxplot(
     axs[-1].tick_params(axis="y", which="both", left=False)
     if y_label is not None:
         axs[0].set_ylabel(y_label)
+
+
+def figure_1():
+    methods_order = ["velocyto", "cellranger"]
+    datasets_order = ["stewart", "fu"]
+    left_label_x = 0.007
+
+    # Load all DataFrames
+    total_counts = read_dataframes("total_counts", datasets=datasets_order)
+    counts_diff = read_dataframes("counts_diff", datasets=datasets_order)
+    counts_corr = read_dataframes("counts_corr_cellranger", datasets=datasets_order)
+    counts_corr = counts_corr[counts_corr.alignment_type != "both"]
+    spliced_unspliced_ratios = read_dataframes(
+        "spliced_unspliced_ratios", datasets=datasets_order
+    )
+    for df in [total_counts, counts_diff, counts_corr, spliced_unspliced_ratios]:
+        df.loc[df.dataset == "fu", "dataset"] = (
+            df.loc[df.dataset == "fu", "dataset"].astype(str)
+            + "_"
+            + df.loc[df.dataset == "fu", "alignment_type"].str.lower()
+        )
+    overlapping_genes = read_dataframes("gene_overlaps", datasets=datasets_order)
+
+    adata = sc.read_h5ad("../data/stewart/adata/adata_raw_velocyto.h5ad")
+
+    # Subset to genes with mean expression >= 1
+    overlapping_genes = overlapping_genes[
+        np.asarray(adata[:, overlapping_genes.Gene_x].X.mean(axis=0) >= 1).ravel()
+        & (overlapping_genes.alignment_type != "both")
+    ]
+
+    overlapping_genes_scatter = (
+        overlapping_genes[overlapping_genes.dataset == "stewart"]
+        .sort_values(by=["overlap_relative_gene_x", "overlap_bases"], ascending=False)
+        .reset_index(drop=True)
+    )
+
+    datasets_order = ["stewart", "fu_se", "fu_pe"]
+
+    fig = plt.figure(figsize=(FIG_WIDTH, FIG_HEIGHT_MAX), layout="constrained")
+    sub_figs = fig.subfigures(3, 1, height_ratios=[7, 7, 12])
+
+    # Top plots
+    sub_figs_top = sub_figs[0].subfigures(1, 4)
+    axs_top = [sub_fig.subplots(1, 1) for sub_fig in sub_figs_top]
+    sns.boxplot(
+        total_counts[total_counts.method.isin(methods_order)],
+        x="dataset",
+        y="counts",
+        hue="method",
+        hue_order=methods_order,
+        order=datasets_order,
+        palette=[PALETTE[0], PALETTE[4]],
+        showfliers=False,
+        log_scale=True,
+        ax=axs_top[0],
+        **BOXPLOT_PROPS,
+    )
+    axs_top[0].set_ylabel("counts per cell")
+
+    sns.boxplot(
+        counts_diff[counts_diff.method.isin(methods_order)],
+        x="dataset",
+        y="difference",
+        palette=[PALETTE[0]],
+        order=datasets_order,
+        showfliers=False,
+        ax=axs_top[1],
+        **BOXPLOT_PROPS,
+    )
+    axs_top[1].axhline(0, c="grey", linestyle=":")
+
+    sns.boxplot(
+        counts_corr[
+            counts_corr.method.isin(methods_order)
+            & counts_corr.dataset.isin(datasets_order)
+        ],
+        x="dataset",
+        y="pearsonr",
+        palette=[PALETTE[0]],
+        order=datasets_order,
+        showfliers=False,
+        ax=axs_top[2],
+        **BOXPLOT_PROPS,
+    )
+    axs_top[2].set_ylabel("Pearson corr.")
+
+    spliced_unspliced_ratios.ratio = spliced_unspliced_ratios.ratio * 100
+    sns.boxplot(
+        spliced_unspliced_ratios[
+            spliced_unspliced_ratios.method.isin(methods_order)
+            & spliced_unspliced_ratios.dataset.isin(datasets_order)
+            & (spliced_unspliced_ratios.genes == "all")
+            & (spliced_unspliced_ratios.splice_state == "unspliced")
+        ],
+        x="dataset",
+        y="ratio",
+        palette=[PALETTE[0]],
+        order=datasets_order,
+        showfliers=False,
+        ax=axs_top[3],
+        **BOXPLOT_PROPS,
+    )
+    axs_top[3].set_ylabel("% unspliced")
+
+    for ax in axs_top:
+        ax.set_xlabel("")
+        ax.set_xticks(
+            ticks=ax.get_xticks(),
+            labels=[DATASETS_LABELS[lab.get_text()] for lab in ax.get_xticklabels()],
+            rotation=45,
+            ha="right",
+            rotation_mode="anchor",
+        )
+        if ax.get_legend():
+            ax.get_legend().remove()
+
+    axs_top[0].text(
+        left_label_x,
+        0.98,
+        "A",
+        transform=mpl.transforms.blended_transform_factory(
+            sub_figs[0].transSubfigure, sub_figs_top[0].transSubfigure
+        ),
+        **AXLAB_KWS,
+    )
+    for i, label in enumerate(string.ascii_uppercase[1 : len(sub_figs_top)]):
+        axs_top[i + 1].text(
+            0.02,
+            0.98,
+            label,
+            transform=sub_figs_top[i + 1].transSubfigure,
+            **AXLAB_KWS,
+        )
+
+    handles, labels = axs_top[0].get_legend_handles_labels()
+    sub_figs[0].legend(
+        handles,
+        [METHODS_DICT[lab] for lab in labels],
+        loc="outside lower center",
+        ncol=2,
+        frameon=False,
+    )
+
+    # Center plots
+    axs_center = sub_figs[1].subplots(1, 2)
+
+    # Positions of overlapping transcripts RPL35 (+) and IQCG (-)
+    transcript_rpl35a = [197_950_190, 197_956_610]
+    exons_rpl35a = [
+        [197_950_190, 197_950_221],
+        [197_950_936, 197_950_936],
+        [197_951_159, 197_951_311],
+        [197_954_003, 197_954_147],
+        [197_955_750, 197_956_610],
+    ]
+    cds_rpl35a = [
+        [197_950_968, 197_950_978],
+        [197_951_159, 197_951_311],
+        [197_954_003, 197_954_147],
+        [197_955_750, 197_955_770],
+    ]
+
+    transcript_iqcg = [197_943_778, 197_959_991]  # real start: 197_889_077
+    exons_iqcg = [
+        [197_959_845, 197_959_991],
+        [197_959_529, 197_959_719],
+        [197_945_620, 197_945_686],
+        [197_943_778, 197_944_051],
+        # [197_938_548, 197_938_780],
+        # [197_932_174, 197_932_302],
+        # [197_926_024, 197_926_105],
+        # [197_913_881, 197_914_042],
+        # [197_912_675, 197_912_749],
+        # [197_892_628, 197_892_759],
+        # [197_891_446, 197_891_547],
+        # [197_889_077, 197_889_714],
+    ]
+    cds_iqcg = [
+        [197_945_620, 197_945_627],
+        [197_943_778, 197_944_051],
+        # [197_938_548, 197_938_780],
+        # [197_932_174, 197_932_302],
+        # [197_926_024, 197_926_105],
+        # [197_913_881, 197_914_042],
+        # [197_912_675, 197_912_749],
+        # [197_892_628, 197_892_759],
+        # [197_891_446, 197_891_547],
+        # [197_889_583, 197_889_714],
+    ]
+
+    y_pos = 0.2
+    x_quiver = np.linspace(transcript_rpl35a[0], transcript_rpl35a[1], 10)
+    y_quiver = np.ones_like(x_quiver) * y_pos
+    axs_center[0].quiver(
+        x_quiver[:-1],
+        y_quiver[:-1],
+        x_quiver[1:] - x_quiver[:-1],
+        y_quiver[1:] - y_quiver[:-1],
+        headaxislength=3,
+        headlength=3,
+        color="grey",
+    )
+    axs_center[0].plot(
+        np.asarray(exons_rpl35a).T,
+        [y_pos, y_pos],
+        lw=10,
+        c=mpl.colormaps["tab20"].colors[1],
+    )
+    axs_center[0].plot(
+        np.asarray(cds_rpl35a).T,
+        [y_pos, y_pos],
+        lw=10,
+        c=mpl.colormaps["tab20"].colors[0],
+    )
+    axs_center[0].text(
+        (transcript_rpl35a[0] + transcript_rpl35a[1]) / 2,
+        y_pos + 0.2,
+        "RPL35A",
+        va="bottom",
+        ha="center",
+        size=8,
+    )
+    x_quiver = np.linspace(transcript_iqcg[1], transcript_iqcg[0], 20)
+    y_quiver = np.ones_like(x_quiver) * (-y_pos)
+    axs_center[0].quiver(
+        x_quiver[:-1],
+        y_quiver[:-1],
+        x_quiver[1:] - x_quiver[:-1],
+        y_quiver[1:] - y_quiver[:-1],
+        headaxislength=3,
+        headlength=3,
+        color="grey",
+    )
+    axs_center[0].plot(
+        [transcript_iqcg[0] - 1_000, transcript_iqcg[0]],
+        [-y_pos, -y_pos],
+        c="grey",
+        ls=":",
+    )
+    axs_center[0].plot(
+        np.asarray(exons_iqcg).T,
+        [-y_pos, -y_pos],
+        lw=10,
+        c=mpl.colormaps["tab20"].colors[3],
+    )
+    axs_center[0].plot(
+        np.asarray(cds_iqcg).T,
+        [-y_pos, -y_pos],
+        lw=10,
+        c=mpl.colormaps["tab20"].colors[2],
+    )
+    axs_center[0].text(
+        (transcript_iqcg[0] + transcript_iqcg[1]) / 2,
+        -y_pos - 0.2,
+        "IQCG",
+        va="top",
+        ha="center",
+        size=8,
+    )
+    # Draw forward and reverse strand
+    xlim = np.asarray(axs_center[0].get_xlim())
+    arrow_len = xlim[1] - xlim[0]
+    axs_center[0].quiver(
+        xlim,
+        np.asarray([0.8, -0.8]),
+        np.asarray([arrow_len, -arrow_len]),
+        np.zeros(2),
+        angles="xy",
+        scale_units="xy",
+        scale=1,
+        headwidth=4,
+        headlength=4,
+        headaxislength=4,
+        color="k",
+    )
+    axs_center[0].text(
+        (xlim[0] + xlim[1]) / 2, 0.9, "forward strand", va="bottom", ha="center", size=8
+    )
+    axs_center[0].text(
+        (xlim[0] + xlim[1]) / 2, -0.9, "reverse strand", va="top", ha="center", size=8
+    )
+    axs_center[0].legend(
+        [
+            (
+                mpl.patches.Rectangle(
+                    (0, 0),
+                    2,
+                    1,
+                    facecolor=mpl.colormaps["tab20"].colors[i],
+                    edgecolor=None,
+                ),
+                mpl.patches.Rectangle(
+                    (0, 0),
+                    2,
+                    1,
+                    facecolor=mpl.colormaps["tab20"].colors[i + 2],
+                    edgecolor=None,
+                ),
+            )
+            for i in [0, 1]
+        ],
+        ["CDS", "UTR"],
+        handler_map={tuple: mpl.legend_handler.HandlerTuple(ndivide=None, pad=0.3)},
+        loc="upper center",
+        bbox_to_anchor=(0.5, -0.1),
+        frameon=False,
+        ncols=2,
+        handlelength=2,
+        handleheight=1.0,
+        handletextpad=0.6,
+    )
+    axs_center[0].axis("off")
+
+    overlapping_genes.loc[overlapping_genes.dataset == "fu", "dataset"] = (
+        overlapping_genes.loc[overlapping_genes.dataset == "fu", "dataset"].astype(str)
+        + "_"
+        + overlapping_genes.loc[
+            overlapping_genes.dataset == "fu", "alignment_type"
+        ].str.lower()
+    )
+    overlapping_genes["overlap"] = pd.cut(
+        overlapping_genes.overlap_relative_gene_x,
+        [0, 0.5, 0.75, 1],
+        right=True,
+        include_lowest=True,
+        labels=[
+            r"overlap$\leq50\%$",
+            r"$50\%<$overlap$\leq75\%$",
+            r"overlap$>75\%$",
+        ],
+    )
+    sns.boxplot(
+        overlapping_genes,
+        x="dataset",
+        y="pearsonr",
+        hue="overlap",
+        palette="Dark2",
+        order=datasets_order,
+        showfliers=False,
+        ax=axs_center[1],
+        **BOXPLOT_PROPS,
+    )
+    axs_center[1].legend(loc="center left", bbox_to_anchor=(1.0, 0.5))
+    axs_center[1].set_xlabel("")
+    axs_center[1].set_ylabel("Pearson corr.")
+    axs_center[1].set_xticks(
+        ticks=axs_center[1].get_xticks(),
+        labels=[
+            DATASETS_LABELS[lab.get_text()] for lab in axs_center[1].get_xticklabels()
+        ],
+        rotation=45,
+        ha="right",
+        rotation_mode="anchor",
+    )
+    sub_figs[1].text(
+        left_label_x, 1, "E", transform=sub_figs[1].transSubfigure, **AXLAB_KWS
+    )
+    axs_center[1].text(
+        -0.2,
+        1,
+        "F",
+        transform=mpl.transforms.blended_transform_factory(
+            axs_center[1].transAxes, sub_figs[1].transSubfigure
+        ),
+        **AXLAB_KWS,
+    )
+
+    # Bottom plots
+    axs_bottom = sub_figs[2].subplots(2, 4)
+    for i, row in overlapping_genes_scatter.head(8).iterrows():
+        gene_x = row["Gene_x"]
+        gene_y = row["Gene_y"]
+        rho = row["pearsonr"]
+        ax = axs_bottom.flat[i]
+        ax.scatter(
+            adata[:, gene_x].X.toarray().ravel(),
+            adata[:, gene_y].layers["unspliced"].toarray().ravel(),
+            s=0.5,
+            c="k",
+            rasterized=True,
+        )
+        ax.text(
+            0.05,
+            0.95,
+            f"$\\rho={rho:.2f}$",
+            size=8,
+            ha="left",
+            va="top",
+            transform=ax.transAxes,
+        )
+        ax.set_xlabel(gene_x)
+        ax.set_ylabel(gene_y)
+    sub_figs[2].text(
+        left_label_x, 1.03, "G", transform=sub_figs[2].transSubfigure, **AXLAB_KWS
+    )
+    sub_figs[2].supxlabel("Cell Ranger counts")
+    sub_figs[2].supylabel("velocyto unspliced counts")
+
+    fig.savefig(os.path.join(FIG_DIR, "fig1"))
+    plt.close()
 
 
 def figure_2():
@@ -520,6 +952,7 @@ def supplementary_figure_4():
 
 def main():
     os.makedirs(FIG_DIR, exist_ok=True)
+    figure_1()
     figure_2()
     figure_3()
     supplementary_figure_1()
